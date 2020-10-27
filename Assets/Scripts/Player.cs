2ed5e7d12e45edf917ca2ba3bd461d424c934dd7
Mel Rumsey <melsrumsey@gmail.com>
2020-10-27 15:34:44 -0500

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    CharacterController _cc;
+    [SerializeField]
+    private float _speed = 3.5f;
+    private float _gravity = 1f;
+    private float _yVelocity;
+    private UI_Manager ui_manager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cc = GetComponent<CharacterController>();
+        ui_manager = GameObject.Find("Canvas").GetComponent<UI_Manager>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            ui_manager.Inventory();
+        }
+        
+        MovePlayer();
+    }
+
+    private void MovePlayer() 
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical"); 
+
+
+        Vector3 direction = new Vector3(horizontalInput, 0 , verticalInput);
+        Vector3 velocity = _speed * direction;
+        velocity.y = _yVelocity;
+        _yVelocity -= _gravity; 
+
+        velocity = transform.transform.TransformDirection(velocity);
+
+        _cc.Move(velocity * Time.deltaTime);
+    }
+
+}
---
+++ Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _inventory;
+    public bool inventoryIsOpen = false;
+
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Inventory()
+    {
+        if (inventoryIsOpen)
+        {
+            _inventory.SetActive(false);
+            inventoryIsOpen = false;
+        }
+        else
+        {
+            _inventory.SetActive(true);
+            inventoryIsOpen = true;
+        
+}
+    }
+}
